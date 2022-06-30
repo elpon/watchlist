7d255fd183ac23b212fda055337663b71efd2663
Leo Wong <leoisusing@foxmail.com>
2022-06-30 12:11:28 +0800

    Add index page,using jinja2 to render

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 from flask import Flask
+from flask import render_template
 from flask import url_for
+name = 'Leo Wong'
+movies = [
+    {'title': 'My neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 app = Flask(__name__)
 
 @app.route('/user/<name>')
@@ -7,10 +21,12 @@ def user_page(name):
     return 'User: %s' % name
 
 @app.route('/')
-@app.route('/index')
-@app.route('/home')
-def hello():
-    return 'Welcome to My Watchlist!'
+def index():
+    return render_template('index.html', name=name, movies=movies)
+# @app.route('/index')
+# @app.route('/home')
+# def hello():
+#     return 'Welcome to My Watchlist!'
     # return u'欢迎来到我的Watchlist!'
     # return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
     
